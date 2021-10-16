cb6f54ba12608b6c0dfe27b966064882c782df78
Mehrab Bozorgi <mehrab.bozorgi@gmail.com>
2021-10-17 00:44:53 +0330

    Part 1

---
+++ lib/product_model.dart
@@ -0,0 +1,14 @@
+class ProductModel {
+  final int id;
+  final String name;
+  final String image;
+  final String point;
+  final String price;
+
+  ProductModel(
+      {required this.id,
+      required this.name,
+      required this.image,
+      required this.point,
+      required this.price});
+}
